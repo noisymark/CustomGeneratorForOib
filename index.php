c4a2151f7cb2ac6ea36ea05de533572837287a7e
Marko Pavlović <markopavlovic316@gmail.com>
2023-01-27 16:12:51 +0100

    Implementing options for OIBs to be separated by comma and new line, implementing option to be able to copy output to clipboard

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 require_once 'components/generator.php';
 if($_SERVER['REQUEST_METHOD']==='POST'){
     $text1 = '';
-    $text2 = '';
     $text3 = '';
     $input = $_POST['Unos'];
+    $option = $_POST['checkbox'];
     $array=[];
     $pb=[];
     $rb='';
@@ -15,15 +15,22 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
         $text1 = 'Generated OIB goes here';
         $text2 = 'Every OIB in new line';
     }else{
+        $ima=true;
         for($i=0;$i<((int)$input);$i++){
             $output=[GenerateOIB()];
-            $text1 .= implode('<br>',$output) . '<br>';
+            if($option==='comma'){
+                $text1 .= (implode('',$output) . ',');
+            } elseif($option==='newline'){
+                $text1 .= (implode('',$output) . '<br>');
+            } else{
+                $text1 .= (implode('',$output) . '<br>');
+            }
         }
 }
 } else{
     $text1 = 'Generated OIB goes here';
-    $text2 = 'Every OIB in new line';
     $text3 = '';
+    $ima = false;
 }
 
 ?>
@@ -41,24 +48,51 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
             <div id="lijevidio" class="col-sm-12 col-md-6 col-xl-6 text-center">
             <form method="POST" action="<?=$_SERVER['PHP_SELF']?>">
                 <label class="h4" for="Unos">How many OIBs would you like to generate?</label>
+                <br><br>
+                <input id="input1" type="number" name="Unos" id="Unos" autofocus value="12">
+                <div id="field">
+                <h4>Separated by:</h4>
+                <input type="radio" name="checkbox" id="zarez" value="comma"<?php if($option==='comma'):?>
+              checked="checked"
+            <?php endif;?>>
+                <label class="h4" for="zarez">Comma</label>
                 <br>
-                <input type="text" name="Unos" id="Unos" autofocus>
-                <input class="bg-success text-white" type="submit" value="Generate">
+                <input type="radio" name="checkbox" id="space" value="newline"<?php if($option==='newline' || $option==0):?>
+              checked="checked"
+            <?php endif;?>>
+                <label class="h4" for="space">New line</label>
+                </div>
+                <br>
+                <input id="button" class="bg-success text-white" type="submit" value="Generate">
             </form>
             </div>
             <div id="desnidio" class="col-sm-12 col-md-6 col-xl-6 text-center">
                 <div class="container col-sm-9 col-md-6 col-xl-5">
                     <h3>Results:</h3>
                     <div id="desnopolje" class="container bg-white text-black">
-                    <h4><?=$text1?></h4>
-                    <h4><?=$text2?></h4>
-                    </div>
+                    <h4 id="outputtext" class="text-wrap"><?=$text1?></h4>
+                    </div><?php if ($ima===true) : ?>
+                    <br>
+                    <button onclick="copyText()" class="bg-success text-white" id="copy">Copy to clipboard</button>
+                    <?php endif?>
                 </div>
             </div>
         </div>
         <?php include_once "components/footer.php";?>
     </div>
-    
+    <script>
+        function copyText() {
+            var myText = document.createElement("textarea");
+            myText.value = document.getElementById("outputtext").innerHTML;
+            myText.value = myText.value.replaceAll("<br>"," ");
+            myText.value = myText.value.replaceAll(","," ");
+            document.body.appendChild(myText)
+            myText.focus();
+            myText.select();
+            document.execCommand('copy');
+            document.body.removeChild(myText);
+        }
+    </script>
     <?php include_once "components/scripts.php";?>
 </body>
 </html>
